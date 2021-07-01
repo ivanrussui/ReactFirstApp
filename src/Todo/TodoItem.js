3b504a198696a3f3f5a3fce51f3fcccf7987d2d4
Ivan <fenomen_ny@mail.ru>
2021-07-02 01:45:39 +0300

    checbox button

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function TodoItem({todo}) {
-  return <li>{todo.title}</li>
+function TodoItem({ todo, index }) {
+  return (
+    <li>
+      <span>
+        <input type="checkbox"></input>
+        <strong>{index + 1}</strong>
+        {todo.title}
+      </span>
+
+			<button>&times;</button>
+    </li>
+  );
 }
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number,
+};
+
+export default TodoItem;
