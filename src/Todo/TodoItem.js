93b04942d16da8617f71cd1816310ed174ce183f
Ivan <fenomen_ny@mail.ru>
2021-07-02 22:14:51 +0300

    f

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const styles = {
+	li: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alingItems: 'center',
+		padding: '.5rem 1rem',
+		border: '1px solid #ccc',
+		borderRadius: '4px',
+		marginBottom: '10px',
+		backgroundColor: 'yellow',
+		fontSize: '20px'
+	},
+	input: {
+		marginRight: '5px'
+	}
+}
+
 function TodoItem({ todo, index }) {
   return (
-    <li>
+    <li style={styles.li}>
       <span>
-        <input type="checkbox"></input>
+        <input type="checkbox" style={styles.input}></input>
         <strong>{index + 1}</strong>
+				&nbsp;
         {todo.title}
       </span>
 
-			<button>&times;</button>
+			<button className="rm">&times;</button>
     </li>
   );
 }
